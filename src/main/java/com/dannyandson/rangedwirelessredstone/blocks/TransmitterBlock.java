eea1761d6a116d11a51bde4b8e13dadfb8f23e63
danny <dannydjdk@users.noreply.github.com>
2021-09-23 21:03:14 -0500

    Loot tables, harvest code and tags so blocks actually drop their items when broken. Fixed namespace of data resources.

--- src/main/java/com/dannyandson/rangedwirelessredstone/blocks/TransmitterBlock.java
+++ src/main/java/com/dannyandson/rangedwirelessredstone/blocks/TransmitterBlock.java
@@ -8,6 +8,7 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
@@ -21,6 +22,7 @@ import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.common.ToolAction;
 
 import javax.annotation.Nullable;
 
@@ -91,6 +93,11 @@ public class TransmitterBlock extends BaseEntityBlock {
             return InteractionResult.CONSUME;
         }
         return super.use(blockState, level, pos, player, hand, blockHitResult);
+    }
+
+    @Override
+    public boolean canHarvestBlock(BlockState state, BlockGetter world, BlockPos pos, Player player) {
+        return true;
     }
 
     @Override
